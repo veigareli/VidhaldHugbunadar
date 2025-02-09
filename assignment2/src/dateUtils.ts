11de9d34aea4447046d080616950ac2b6031697c
veigareli <veigareli@gmail.com>
2025-02-09 19:51:09 +0000

    replace moment

--- assignment2/src/dateUtils.ts
+++ assignment2/src/dateUtils.ts
@@ -1,22 +1,37 @@
-import moment from "moment";
+import { addDays, addMonths, addWeeks, addYears, addSeconds, addMinutes, isBefore, isSameDay as isSameDayFn, isWithinInterval } from "date-fns";
 import { DATE_UNIT_TYPES } from "./constants";
 
 export function getCurrentYear(): number {
-  return moment().year();
+  return new Date().getFullYear();
 }
 
 export function add(date: Date, number: number, type: DATE_UNIT_TYPES = DATE_UNIT_TYPES.DAYS): Date {
-  return moment(date).add(number, type).toDate();
+  switch (type) {
+    case DATE_UNIT_TYPES.SECONDS:
+      return addSeconds(date, number);
+    case DATE_UNIT_TYPES.MINUTES:
+      return addMinutes(date, number);
+    case DATE_UNIT_TYPES.DAYS:
+      return addDays(date, number);
+    case DATE_UNIT_TYPES.WEEKS:
+      return addWeeks(date, number);
+    case DATE_UNIT_TYPES.MONTHS:
+      return addMonths(date, number);
+    case DATE_UNIT_TYPES.YEARS:
+      return addYears(date, number);
+    default:
+      throw new Error("Invalid date unit type");
+  }
 }
 
 export function isWithinRange(date: Date, from: Date, to: Date): boolean {
-  return moment(date).isBetween(from, to);
+  return isWithinInterval(date, { start: from, end: to });
 }
 
 export function isDateBefore(date: Date, compareDate: Date): boolean {
-  return moment(date).isBefore(compareDate);
+  return isBefore(date, compareDate);
 }
 
 export function isSameDay(date: Date, compareDate: Date): boolean {
-  return moment(date).isSame(compareDate);
+  return isSameDayFn(date, compareDate);
 }
