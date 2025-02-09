dcfb7a744bca87219b1588739e59e791a00f066a
veigareli <veigareli@gmail.com>
2025-02-09 19:49:43 +0000

    annotations

--- assignment2/src/dateUtils.ts
+++ assignment2/src/dateUtils.ts
@@ -1,22 +1,22 @@
 import moment from "moment";
 import { DATE_UNIT_TYPES } from "./constants";
 
-export function getCurrentYear() {
+export function getCurrentYear(): number {
   return moment().year();
 }
 
-export function add(date, number, type = DATE_UNIT_TYPES.DAYS) {
+export function add(date: Date, number: number, type: DATE_UNIT_TYPES = DATE_UNIT_TYPES.DAYS): Date {
   return moment(date).add(number, type).toDate();
 }
 
-export function isWithinRange(date, from, to) {
+export function isWithinRange(date: Date, from: Date, to: Date): boolean {
   return moment(date).isBetween(from, to);
 }
 
-export function isDateBefore(date, compareDate) {
+export function isDateBefore(date: Date, compareDate: Date): boolean {
   return moment(date).isBefore(compareDate);
 }
 
-export function isSameDay(date, compareDate) {
+export function isSameDay(date: Date, compareDate: Date): boolean {
   return moment(date).isSame(compareDate);
 }
